690534d994e55987886196b594b71a2d4b094826
mortang2410 <mortang2410@totally.mail>
2018-09-16 13:14:23 -0700

    tpm

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/wilder/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/wilder/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/wilder/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/wilder/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
---
+++ env.sh
@@ -0,0 +1,8 @@
+export PATH=$HOME/localpath:$HOME/.local/bin:$PATH:/usr/local/bin:$HOME/.gem/ruby/2.5.0/bin
+
+export JAVA_FONTS=/usr/share/fonts/TTF
+export COWPATH=/usr/share/cowsay/cows:~/cows/
+export KDEDIRS=/usr/local/:$HOME/build/kdevelop_platform_combined_bin/:$KDEDIRS
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib 
+export MYPYPATH="/usr/lib/python36.zip:/usr/lib/python3.6:/usr/lib/python3.6/lib-dynload"
+export XDG_CACHE_HOME=~/.cache
