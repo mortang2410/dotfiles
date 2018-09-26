5cd8beac2caf4748f3d6cde2db8d31d9a95aa7e1
mortang2410 <mortang2410@totally.mail>
2018-09-25 19:48:44 -0700

    added source_check
    Former-commit-id: 9e12153d22a66d75cdea693c69369849861c764d [formerly e6092be06fdb50e6e91387825422ea60d384fbf1] [formerly e6092be06fdb50e6e91387825422ea60d384fbf1 [formerly 72ddf4e8b58aadd0e8eec22407bc310071585823]]
    Former-commit-id: a6d93b1e48e054c25afd4059551c88d7eb18fc2f
    Former-commit-id: aab4c1ec8791eba4d769493855c896ebbecce4fa

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -56,6 +56,67 @@ class my_edit(Command):
         # This is a generic tab-completion function that iterates through the
         # content of the current directory.
         return self._tab_directory_content()
+# ----------------
+#     def source(self, filename):
+#         """:source <filename>
+#
+#         Load a config file.
+#         """
+#         filename = os.path.expanduser(filename)
+#         LOG.debug("Sourcing config file '%s'", filename)
+#         with open(filename, 'r') as fobj:
+#             for line in fobj:
+#                 line = line.strip(" \r\n")
+#                 if line.startswith("#") or not line.strip():
+#                     continue
+#                 try:
+#                     self.execute_console(line)
+#                 except Exception as ex:  # pylint: disable=broad-except
+#                     if ranger.args.debug:
+#                         raise
+#                     self.notify('Error in line `%s\':\n  %s' % (line, str(ex)), bad=True)
+# ------------------                    -----------------
+
+class source_check(Command):
+    # The so-called doc-string of the class will be visible in the built-in
+    # help that is accessible by typing "?c" inside ranger.
+    """:source_check <filename>
+    Check that file exists before sourcing.
+    """
+
+    # The execute method is called when you run this command in ranger.
+    def execute(self):
+        # self.arg(1) is the first (space-separated) argument to the function.
+        # This way you can write ":my_edit somefilename<ENTER>".
+        if self.arg(1):
+            # self.rest(1) contains self.arg(1) and everything that follows
+            target_filename = os.path.expanduser(self.rest(1))
+        else:
+            # self.fm is a ranger.core.filemanager.FileManager object and gives
+            # you access to internals of ranger.
+            # self.fm.thisfile is a ranger.container.file.File object and is a
+            # reference to the currently selected file.
+            target_filename = self.fm.thisfile.path
+
+        # This is a generic function to print text in ranger.  
+        # self.fm.notify("Let's source the file " + target_filename + "!")
+
+        # Using bad=True in fm.notify allows you to print error messages:
+        if not os.path.exists(target_filename):
+            # self.fm.notify("The given file does not exist!", bad=True)
+            return
+
+        # This executes a function from ranger.core.acitons, a module with a
+        # variety of subroutines that can help you construct commands.
+        # Check out the source, or run "pydoc ranger.core.actions" for a list.
+        self.fm.source(target_filename)
+
+    # The tab method is called when you press tab, and should return a list of
+    # suggestions that the user will tab through.
+    def tab(self):
+        # This is a generic tab-completion function that iterates through the
+        # content of the current directory.
+        return self._tab_directory_content()
 
 
 # https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
