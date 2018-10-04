3b620d3a873085fcb6f4eca26d114790c8039a00
wilder <hmkhang24@gmail.com>
2018-10-04 16:47:14 -0700

    removed a few things
    Former-commit-id: 9391d8a8491ba9caeb9491a2bec95e9246852dcc

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/wilder/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/wilder/.fzf/bin"
+if [[ ! "$PATH" == */root/.fzf/bin* ]]; then
+  export PATH="$PATH:/root/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/wilder/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/root/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/wilder/.fzf/shell/key-bindings.zsh"
+source "/root/.fzf/shell/key-bindings.zsh"
 
