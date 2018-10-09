cfa004fba6ade314e8ed67460bd6a41bf3ed88a8
wilder <mortang2410@totally.mail>
2018-10-09 14:58:29 -0700

    polish on WSL
    Former-commit-id: 82e196bd8092ee87bbeec79b2c717edc4cbedb91

--- .zsh.d/modules/auto_fu.zsh
+++ .zsh.d/modules/auto_fu.zsh
@@ -74,7 +74,7 @@ function {
 
   # Start the autosuggestion widgets
   _zsh_autosuggest_start() {
-    _zsh_autosuggest_check_deprecated_config
+    # _zsh_autosuggest_check_deprecated_config
     _zsh_autosuggest_bind_widgets
     add-zsh-hook -d precmd _zsh_autosuggest_start
   }
