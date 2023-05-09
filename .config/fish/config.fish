73b762fdd58bfba085711203038c05f851fd081e
mortang2410 <mortang2410@totally.mail>
2023-05-09 09:26:43 -0400

    massive rewriting and cutting out the fat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+export FZF_DEFAULT_COMMAND='rg --hidden --no-ignore -l ""'
+export FZF_CTRL_T_COMMAND='rg --hidden --no-ignore -l ""'
+export FZF_ALT_C_COMMAND='fd --hidden -t d'
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+export EDITOR='hx'
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
